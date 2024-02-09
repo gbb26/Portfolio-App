2d580e4cd01d8aec35e2231a6216c6182d90dead
unknown <gorakshnath107@gmail.com>
2024-02-09 14:35:26 +0530

    ...

--- src/components/Skills/SkillsTemplate.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SkillsTemplate = (props) => {
-  return (
-    <li className="skill-card skill-name">{props.name}</li>
-  );
-};
-
-export default SkillsTemplate;
